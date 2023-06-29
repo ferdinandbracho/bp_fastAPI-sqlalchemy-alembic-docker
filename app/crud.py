bd9e2ba1ee3bcd1f096cab597fe8c231f7909b56
Ferdinand Bracho <ferdinand.bracho@gmail.com>
2023-06-29 09:32:09 -0600

    feat: udpate crub base class

--- app/crud.py
+++ app/crud.py
@@ -16,7 +16,7 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
 
     def __init__(self, model: Type[ModelType]):
         """
-        CRUD object with default methods to Create, Read, Update, Delete (CRUD).
+        CRUD object with default methods to Create, Read, Update, Delete (CRUD)
         **Parameters**
         * `model`: A SQLAlchemy model class
         * `schema`: A Pydantic model (schema) class
